0f8e38df3d02e057c5b3c64937cccefc9249abaa
Edigleysson Silva <edigleyssonsilva@gmail.com>
2017-05-16 00:46:09 -0300

    Adicionado gitignore

--- config/PagSeguroConfig.php
+++ config/PagSeguroConfig.php
@@ -1,6 +1,6 @@
 <?php 
-	define('TOKEN', 'seu token');
-	define('EMAIL', 'seu email');
+	define('TOKEN', '1A10D3AD9F4741F0A3979A81F6937BDC');
+	define('EMAIL', 'edigleyssonsilva@gmail.com');
 
 	$urls  = [
 		'transaction' => 'https://ws.sandbox.pagseguro.uol.com.br/v2/transactions',
